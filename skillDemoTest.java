87981e2bba8c8470f1b46a864c13a8984cfd4e1a
LXKeee <91454987+LXKeee@users.noreply.github.com>
2022-02-04 03:40:58 -0800

    a correct one

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void minus() {
-        assertEquals(2, skillDemo.minus(2, 1)); //this one should fail
+        assertEquals(1, skillDemo.minus(2, 1)); //this one should be okay
     }
 }
